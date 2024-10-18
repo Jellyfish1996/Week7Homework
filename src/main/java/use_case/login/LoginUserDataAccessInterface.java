72c9578d4066a91351edb8d7a64bd9514470c22f
wyattanderson <wyatt.anderson@mail.utoronto.ca>
2024-10-18 15:16:28 -0400

    Completed task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name the name to set
      */
     void setCurrentUser(String name);
+
+    /**
+     * Get the current User dummy method.
+     * @return String
+     */
+    String getCurrentUser();
 }
